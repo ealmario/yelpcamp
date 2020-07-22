05c1b19a2e1e3af2c7fe714f179ea4c0d680c8b2
ealmario <ee.almario@gmail.com>
2020-07-22 21:02:00 +0800

    Add new campground route

--- app.js
+++ app.js
@@ -5,7 +5,9 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.listen(port, () => {
   console.log(`Yelpcamp is live on port ${port}`);
@@ -15,40 +17,53 @@ app.listen(port, () => {
 // ROUTES
 // =============
 
+const campgrounds = [
+  {
+    name: "Camp Talusi",
+    image: "http://via.placeholder.com/300"
+  },
+  {
+    name: "Backpackers Island",
+    image: "http://via.placeholder.com/300"
+  },
+  {
+    name: "Camp Tent",
+    image: "http://via.placeholder.com/300"
+  },
+  {
+    name: "Camp Aninipot",
+    image: "http://via.placeholder.com/300"
+  },
+  {
+    name: "Ginto Island Camping",
+    image: "http://via.placeholder.com/300"
+  },
+]
+
 app.get('/', (req, res, next) => {
   res.render('landing');
 })
 
 app.get('/campgrounds', (req, res, next) => {
-  const campgrounds = [
-    {
-      name: "Camp Talusi",
-      image: "http://via.placeholder.com/300"
-    },
-    {
-      name: "Backpackers Island",
-      image: "http://via.placeholder.com/300"
-    },
-    {
-      name: "Camp Tent",
-      image: "http://via.placeholder.com/300"
-    },
-    {
-      name: "Camp Aninipot",
-      image: "http://via.placeholder.com/300"
-    },
-    {
-      name: "Ginto Island Camping",
-      image: "http://via.placeholder.com/300"
-    },
-  ]
   res.render('campgrounds', {campgrounds: campgrounds});
 })
 
 app.post('/campgrounds', (req, res, next) => {
-  res.send("POST ROUTE");
-  // get date from form and add to campgrounds array
+  // get data from form and add to campgrounds array
   // redirect back to campgrounds page
+  const newCampground = 
+  {
+    name: req.body.name,
+    image: req.body.img
+  }
+
+  campgrounds.push(newCampground);
+  res.redirect('/campgrounds');
+})
+
+app.get('/campgrounds/new', (req, res, next) => {
+  //  form
+  res.render('new');
 })
 
 app.get('*', (req, res, next) => {
